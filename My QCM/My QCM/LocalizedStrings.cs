f6d34e0a18fdcdcf66706c8d868f67e48de6c645
jeoffrey Godart <jeoffrey.godart@hotmail.fr>
2016-03-15 15:10:08 +0100

    Project Creation.

---
+++ My QCM/My QCM/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using My_QCM.Resources;
+
+namespace My_QCM
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
